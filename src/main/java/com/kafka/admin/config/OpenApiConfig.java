3c2e499ba4b8ba66aac04bc96ccc88b7abb38834
galgrayzer <galgrayzer@gmail.com>
2026-05-03 11:01:25 +0300

    Default bootstrap.servers now the default option on swagger ui

--- src/main/java/com/kafka/admin/config/OpenApiConfig.java
+++ src/main/java/com/kafka/admin/config/OpenApiConfig.java
@@ -3,6 +3,9 @@ package com.kafka.admin.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +19,9 @@ public class OpenApiConfig {
     @Value("${springdoc.api-docs.version:1.0.0}")
     private String apiVersion;
 
+    @Autowired
+    private KafkaAdminConfig kafkaAdminConfig;
+
     @Bean
     public OpenAPI kafkaAdminOpenAPI() {
         return new OpenAPI()
@@ -26,5 +32,29 @@ public class OpenApiConfig {
                         .contact(new Contact()
                                 .name("Kafka Admin Team")
                                 .email("admin@kafka.local")));
+    }
+
+    @Bean
+    public OpenApiCustomizer bootstrapServersExampleCustomizer() {
+        return openAPI -> {
+            String defaultBootstrapServers = kafkaAdminConfig.getDefaultBootstrapServers();
+            var paths = openAPI.getPaths();
+            if (paths == null) return;
+
+            paths.forEach((path, pathItem) -> {
+                if (pathItem == null) return;
+                pathItem.readOperations().forEach(operation -> {
+                    if (operation.getParameters() == null) return;
+                    operation.getParameters().forEach(param -> {
+                        if (param instanceof Parameter) {
+                            Parameter parameter = (Parameter) param;
+                            if ("bootstrapServers".equals(parameter.getName())) {
+                                parameter.setExample(defaultBootstrapServers);
+                            }
+                        }
+                    });
+                });
+            });
+        };
     }
 }
